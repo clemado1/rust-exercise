e7c99dce33cf9cf224375480f84e13f081e329af
clemado1 <clemado1@gmail.com>
2019-10-06 08:26:30 +0900

    Chapter 09.

--- src/ch02.rs
+++ src/ch02.rs
@@ -2,6 +2,24 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -16,10 +34,14 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        let g = Guess::new(guess);
+
+        let guess = g.value();
 
         println!("You guessed: {}", guess);
 
---
+++ src/ch09.rs
@@ -0,0 +1,65 @@
+use std::error::Error;
+use std::fs::{self, File};
+use std::io::{self, ErrorKind, Read};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let v = vec![1, 2, 3];
+
+    //v[99]; RUST_BACKTRACE=1 cargo run
+
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    /*
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+    */
+
+    //let f = File::open("hello2.txt").unwrap();
+    let f = File::open("hello2.txt").expect("Failed to open hello2.txt");
+
+    Ok(())
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
