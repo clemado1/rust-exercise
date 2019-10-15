4620c248e93d0c96bde4702d8b59be8b9f781c3d
clemado1 <clemado1@gmail.com>
2019-10-15 23:53:18 +0900

    Chapter 13-2.

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,76 @@
-use std::thread;
-use std::time::Duration;
-
-struct Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    calculation: T,
-    value: Option<u32>,
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
 }
 
-impl<T> Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: None,
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
-
-fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
-
-    generate_workout(simulated_user_specified_value, simulated_random_number);
-
-    let x = 4;
-    let equal_to_x = |z| z == x;
-    let y = 4;
-
-    assert!(equal_to_x(y));
-
-    let xy = vec![1, 2, 3];
-    let equal_to_xy = move |zx| zx == xy;
-    let yz = vec![1, 2, 3];
-
-    println!("can't use x here: {:?}", x);
-
-    assert!(equal_to_xy(yz));
-}
-
-fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_result = Cacher::new(|num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_result.value(intensity));
-        println!("Next, do {} situps!", expensive_result.value(intensity));
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_result.value(intensity)
-            );
-        }
-    }
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    //into_iter 소유권을 갖는 반복자
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
 #[test]
-fn call_with_different_values() {
-    let mut c = Cacher::new(|a| a);
+fn filters_by_size() {
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        },
+        Shoe {
+            size: 10,
+            style: String::from("boot"),
+        },
+    ];
 
-    let v1 = c.value(1);
-    let v2 = c.value(2);
+    let in_my_size = shoes_in_my_size(shoes, 10);
 
-    assert_eq!(v2, 2);
+    assert_eq!(
+        in_my_size,
+        vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker")
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot")
+            },
+        ]
+    )
+}
+
+fn main() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    v1.iter().map(|x| x + 1);
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum(); //소유권 변경
+
+    assert_eq!(total, 6)
 }
