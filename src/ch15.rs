6ebf3d247db2e62f975dd446f2a133a5d5514f4e
clemado1 <clemado1@gmail.com>
2019-10-19 23:44:26 +0900

    Chapter 15-4.

--- src/ch15.rs
+++ src/ch15.rs
@@ -1,9 +1,16 @@
 use crate::List::{Cons, Nil};
 use std::mem::drop;
 use std::ops::Deref;
+use crate::List2::{Cons, Nil};
+use std::rc::Rc;
 
 enum List {
     Cons(i32, Box<List>),
+    Nil,
+}
+
+enum List2 {
+    Cons(i32, Rc<List2>),
     Nil,
 }
 
@@ -70,6 +77,16 @@ fn main() {
     drop(c);
 
     println!("CustomSmartPointers created.");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 fn hello(name: &str) {
