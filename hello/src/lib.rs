219030aff556374df22f7a982daa3fa8b19dda5f
clemado1 <clemado1@gmail.com>
2019-11-09 23:33:59 +0900

    Chapter 20-2.

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,11 +1,44 @@
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<Self>) {
+        (*self)()
+    }
+}
+
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 #[derive(Debug)]
 pub struct PoolCreationError;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+type Job = Box<dyn FnBox + Send + 'static>;
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {} got a job; executing.", id);
+
+            job.call_box();
+        });
+        Worker { id, thread }
+    }
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool
@@ -26,13 +59,15 @@ impl ThreadPool {
         if size <= 0 {
             return Err(PoolCreationError);
         }
-        let mut threads = Vec::with_capacity(size);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            //
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        let pool = ThreadPool { threads };
+        let pool = ThreadPool { workers, sender };
 
         Ok(pool)
     }
@@ -41,5 +76,8 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
