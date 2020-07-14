c45cccf687e0878a2004ea1b70e9b1b8cff2e861
clemado1 <clemado1@gmail.com>
2020-07-14 23:59:12 +0900

    leetcode Reverse Integer

--- leetcode/src/main.rs
+++ leetcode/src/main.rs
@@ -1,1 +1,23 @@
-fn main() {}
+fn main() {
+    reverse(100);
+}
+
+fn reverse(x: i32) -> i32 {
+    let mut number = x;
+    let mut result = 0;
+    let mut multi = 1;
+
+    let mut numbers: Vec<i32> = Vec::new();
+
+    while number != 0 {
+        numbers.push(number % 10);
+        number /= 10;
+
+    }
+
+    for (i, n) in numbers.iter().enumerate(){
+        result += i32::pow(*n, (numbers.len()-1-i).try_into().unwrap());
+    }
+
+    result
+}
