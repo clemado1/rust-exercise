3f4b36aedb35bb6bc4b5856295befb33ac9669a6
clemado1 <clemado1@gmail.com>
2020-11-08 23:48:55 +0900

    d1 Results and Options

--- d1_filework/src/main.rs
+++ d1_filework/src/main.rs
@@ -4,6 +4,7 @@ use serde_derive::*;
 pub enum TransactionError {
     LoadError(std::io::Error),
     ParseError(serde_json::Error),
+    Mess(&'static str),
 }
 
 impl From<std::io::Error> for TransactionError {
@@ -18,6 +19,12 @@ impl From<serde_json::Error> for TransactionError {
     }
 }
 
+impl From<&'static str> for TransactionError {
+    fn from(e: &'static str) -> Self {
+        TransactionError::Mess(e)
+    }
+}
+
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Transaction {
     from : String,
@@ -25,12 +32,28 @@ pub struct Transaction {
     amount: u64,
 }
 
-fn main() {
+fn main() -> Result<(), TransactionError> {
     println!("Hello, world!");
     let trans = get_transactions("test_data/transactions.json").expect("Could not load transactions");
     for t in trans {
         println!("{:?}", t);
     }
+
+    // First trans
+    let t = get_first_transaction_for("test_data/transactions.json", "Matt4").ok_or("Could not get first transaction")?;
+    println!("Matt's first transaction: {:?}", t);
+
+    Ok(())
+}
+
+fn get_first_transaction_for(fname: &str, uname: &str) -> Option<Transaction> {
+    let trans = get_transactions(fname).ok()?;
+    for t in trans {
+        if t.from == uname {
+            return Some(t);
+        }
+    }
+    None
 }
 
 fn get_transactions(fname: &str) -> Result<Vec<Transaction>, TransactionError> {
