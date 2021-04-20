649d19fdab9b41b818eeb71d554e5a5de694bd19
clemado1 <clemado1@gmail.com>
2021-04-20 22:59:12 +0900

    v4 Channels and Thread clemado1 Yesterday

--- d3_lifetimes/src/v4_channels.rs
+++ d3_lifetimes/src/v4_channels.rs
@@ -2,10 +2,11 @@ use std::time::Duration;
 use std::sync::{Arc, Mutex};
 
 fn main() {
-    with_arc();
+    // with_arc();
+    with_channels();
 }
 
-fn with_arc() {
+pub fn with_arc() {
     // let mut m = 5;
     // ensure not holding same object same time across thread.
     let m = Arc::new(Mutex::new(String::from("moving")));
@@ -21,4 +22,46 @@ fn with_arc() {
     println!("This is the initial thread");
     let s = m.lock().unwrap();
     println!("now m = {}", s);
+}
+
+fn with_channels() {
+    // return two part of channel, sender and receiver.
+    // MPSC stand for multi producer single consumer.
+    let (ch_s, ch_r) = std::sync::mpsc::channel::<Box<dyn Fn(&mut String) + Send>>();
+
+    // empty tuple. make sure doesn't waste extra usage
+    let (done_s, done_r) = std::sync::mpsc::channel::<()>();
+
+    std::thread::spawn(move || {
+        let mut hidden = String::new();
+        loop {
+            match ch_r.recv() {
+                Ok(f) => {
+                    f(&mut hidden);
+                    println!("hidden = {}", hidden);
+                }
+                Err(e) => {
+                    println!("Done");
+                    done_s.send(());
+                    return;
+                }
+            }
+        }
+    });
+
+    ch_s.send(Box::new(|s: &mut String| {
+        s.push_str("Hello");
+    })).unwrap();
+
+    let ch_2 = ch_s.clone();
+
+    ch_2.send(Box::new(|s: &mut String| {
+        s.push_str(" world");
+    })).unwrap();
+
+    drop(ch_s);
+    drop(ch_2);
+
+    // std::thread::sleep(Duration::from_millis(1000));
+    done_r.recv().ok();
 }
