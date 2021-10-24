b5a821cf6725c2cf4c8eac3c24abff5057bc57df
clemado1 <clemado1@gmail.com>
2021-10-24 23:37:04 +0900

    d4 Reading Properties in Derive Macros

--- d4_builder_derive/src/lib.rs
+++ d4_builder_derive/src/lib.rs
@@ -2,6 +2,45 @@ extern crate proc_macro;
 
 use proc_macro::{TokenStream, TokenTree};
 
+#[derive(Debug)]
+struct Row {
+    name: String,
+    ttype: String,
+}
+
+fn parse_row<I: Iterator<Item=TokenTree>>(i: &mut I) -> Option<Row> {
+    let mut name = i.next()?.to_string();
+    if &name == "pub" {
+        name = i.next()?.to_string();
+    }
+    i.next()?; //colon
+
+    let mut ttype = String::new();
+    let mut d = 0;
+    loop {
+        match i.next() {
+            None => return Some(Row { name, ttype }),
+            Some(TokenTree::Punct(p)) => match p.as_char() {
+                ',' => if d == 0 {
+                    return Some(Row { name, ttype });
+                } else {
+                    ttype.push(',');
+                },
+                '<' | '(' => {
+                    d += 1;
+                    ttype.push(p.as_char());
+                },
+                '>' | ')' => {
+                    d -= 1;
+                    ttype.push(p.as_char());
+                },
+                c => ttype.push(c),
+            },
+            Some(v) => ttype.push_str(&v.to_string()),
+        }
+    }
+}
+
 #[proc_macro_derive(Setter)]
 pub fn setter_derive(input: TokenStream) -> TokenStream {
     let mut top = input.into_iter();
@@ -13,12 +52,35 @@ pub fn setter_derive(input: TokenStream) -> TokenStream {
 
     let name = top.next().unwrap();
 
-    format!(
-        "impl {} {{
-            fn dothing(&self) {{
-                println!(\"Doing thing with {0}\");
+    let mut row_iter = if let Some(TokenTree::Group(g)) = top.next() {
+        g.stream().into_iter()
+    } else {
+        panic!();
+    };
+
+    let mut rows = Vec::new();
+
+    while let Some(r) = parse_row(&mut row_iter) {
+        eprintln!("Row = {:?}", r);
+        rows.push(r);
+    }
+
+    let lines: String = rows.iter().map(|r| {
+        format!("
+            fn set_{0}(&mut self, v:{1}) {{
+                self.{0} = v;
             }}
-    }}", name).parse().unwrap()
+            ", r.name, r.ttype)
+    }).collect();
+
+    let res = format!(
+        "impl {} {{
+            {}
+        }}"
+        , name, lines);
+
+    eprintln!("Res == {}", res);
+    res.parse().unwrap()
 }
 
 #[cfg(test)]
